3b71ddddaac358e0edb3fd1548508e37cfccea9c
Gustavo <gustamvieira@gmail.com>
2024-05-09 14:07:37 -0300

    primeiro metodo de formulario

--- formularios/views.py
+++ formularios/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('teste')
+    return render(request, 'home.html')
+
+def processa_formulario(request):
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    return HttpResponse(f'{nome} {email}')
